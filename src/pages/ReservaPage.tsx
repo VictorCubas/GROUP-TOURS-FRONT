45cda047b9be2a9455ad6eafc125dcd473033db4
Victor Cubas <vhcubas91@gmail.com>
2025-09-17 07:14:20 -0300

    [chore] se corrige las importaciones

--- src/pages/ReservaPage.tsx
+++ src/pages/ReservaPage.tsx
@@ -76,7 +76,7 @@ import { ToastContext } from "@/context/ToastContext"
 import Modal from "@/components/Modal"
 import { IoCheckmarkCircleOutline, IoWarningOutline } from "react-icons/io5";
 import ResumenCardsDinamico from "@/components/ResumenCardsDinamico"
-import { GenericSearchSelect } from "@/components/SimpleSearchSelect"
+import { GenericSearchSelect } from "@/components/GenericSearchSelect"
 import { useSessionStore } from "@/store/sessionStore"
 import placeholderViaje from "@/assets/paquete_default.png";
 import { Checkbox } from "@/components/ui/checkbox"
