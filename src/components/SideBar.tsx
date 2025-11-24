9d9bdb277db9ffa1983b467d488e6bc3b0fcfd0b
Victor Cubas <vhcubas91@gmail.com>
2025-11-24 07:37:01 -0300

    ajustes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -60,7 +60,7 @@ const sidebarItems = [
     href: "#",
     color: "text-blue-400",
     submenu: [
-      { label: "Facturación", href: "/facturacion/facturas", color: "text-emerald-400", bgcolor: "bg-emerald-400" },
+      { label: "Facturación", href: "/facturacion/facturacion", color: "text-emerald-400", bgcolor: "bg-emerald-400" },
       // { label: "Aperturas", href: "/arqueo/aperturas", color: "text-orange-400", bgcolor: "bg-orange-400" },
       // { label: "Cierres", href: "/arqueo/cierres", color: "text-pink-400", bgcolor: "bg-pink-400" },
       // { label: "Movimientos", href: "/arqueo/movimientos", color: "text-yellow-400", bgcolor: "bg-yellow-400" },
